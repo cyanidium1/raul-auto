3a7b688688590d5503c35ca0e789abe31e35ca88
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2024-06-03 14:55:28 +0300

    "Added link to manifest.json in RootLayout"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="manifest" href="/manifest.json"></link>
       <body className={myFontPanagram.className}>
         <Header />
         <main>{children}</main>
